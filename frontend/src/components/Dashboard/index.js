911a9903271d916e51bf4e0a7571e7ca280d854a
Thanh Nguyen <thanhnguyenttn@gmail.com>
2020-12-29 19:23:07 -0500

    Update to / and /dashboard - Work on dashboard

---
+++ frontend/src/components/Dashboard/index.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+// import { Redirect } from "react-router-dom";
+import * as sessionActions from "../../store/session";
+import './Dashboard.css';
+
+function Dashboard() {
+  return (
+    <div className="posts">
+      < h1 > Hello from dashboard</h1 >
+    </div>
+)
+}
+
+export default Dashboard;
