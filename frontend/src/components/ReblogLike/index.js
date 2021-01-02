17f448c2b6745423138743a661218d1d4602480d
Thanh Nguyen <thanhnguyenttn@gmail.com>
2021-01-01 22:54:04 -0500

    Added 'Coming Soon' to reblog/like button

--- frontend/src/components/ReblogLike/index.js
+++ frontend/src/components/ReblogLike/index.js
@@ -1,11 +1,36 @@
 import "./ReblogLike.css";
+import { useState, useEffect } from "react";
 
 
 const ReblogLike = () => {
+  const [showDiv, setShowDiv] = useState(false);
+
+  const openMenu = () => {
+    if (showDiv) return;
+    setShowDiv(true);
+  };
+
+  useEffect(() => {
+    if (!showDiv) return;
+
+    const closeMenu = () => {
+      setShowDiv(false);
+    };
+
+    document.addEventListener('click', closeMenu);
+
+    return () => document.removeEventListener("click", closeMenu);
+  }, [showDiv]);
+
   return (
     <div className="footer">
-      <button>LIKE</button>
-      <button>REBLOG</button>
+      <button onClick={openMenu}>LIKE</button>
+      <button onClick={openMenu}>REBLOG</button>
+      { showDiv && (
+        <div className="like-dropdown">
+          Coming Soon! 
+        </div>
+      )}
     </div>
   )
 }
